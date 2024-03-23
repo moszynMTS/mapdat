75be7b60438a36f266cda748a82989dc6bd0b09e
ObalonyPiwot <74457736+ObalonyPiwot@users.noreply.github.com>
2024-03-23 16:34:43 +0100

    Delete src directory

--- src/MapDat.AuthorizationServer/obj/Debug/netcoreapp3.1/42g5aymn.sql
+++
@@ -1,9 +0,0 @@
-﻿ALTER TABLE [AspNetUsers] ADD [Lp] int NOT NULL DEFAULT 0;
-
-GO
-
-INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-VALUES (N'20231123140311_AddLpToUser', N'3.1.4');
-
-GO
-
